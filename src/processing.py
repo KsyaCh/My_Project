7ba94aa28e949547438189973f7187295bb682b3
Ksenya Chernytsyna <ksenyamebel@yandex.ru>
2025-05-26 20:53:54 +0400

    добавление ветки

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,7 @@ def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
     return result
 
 
+def sort_by_date(list_of_dict: list, reverse=True) -> list:
+    """функция, сортирующая данные в списке по дате"""
+    sorted_list_of_dict = sorted(list_of_dict, key=lambda x: x['date'], reverse=reverse)
+    return sorted_list_of_dict
